1fe65faf9ff4f6c0648f9b89af2811bc05f75cda
Dmitry Durnov <dmitry.durnov@intel.com>
2021-06-28 21:49:42 +0300

    Add missing files for last sync, 26.06.21

---
+++ src/coll/coll_check.cpp
@@ -0,0 +1,179 @@
+/*
+ Copyright 2016-2020 Intel Corporation
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#include <cstdint>
+#include <numeric>
+
+#include "coll/coll.hpp"
+#include "coll/coll_check.hpp"
+#include "common/env/env.hpp"
+#include "common/global/global.hpp"
+
+#ifdef CCL_ENABLE_SYCL
+std::string ccl_usm_type_to_str(sycl::usm::alloc type) {
+    switch (type) {
+        case sycl::usm::alloc::host: return "host";
+        case sycl::usm::alloc::device: return "device";
+        case sycl::usm::alloc::shared: return "shared";
+        case sycl::usm::alloc::unknown: return "unknown";
+        default: CCL_THROW("unexpected USM type: ", static_cast<int>(type));
+    }
+}
+
+std::string ccl_sycl_device_to_str(const sycl::device& dev) {
+    if (dev.is_host()) {
+        return "host";
+    }
+    else if (dev.is_cpu()) {
+        return "cpu";
+    }
+    else if (dev.is_gpu()) {
+        return "gpu";
+    }
+    else if (dev.is_accelerator()) {
+        return "accel";
+    }
+    else {
+        CCL_THROW("unexpected device type");
+    }
+}
+
+void ccl_check_usm_pointers(const ccl_coll_param& param) {
+    auto bufs = param.get_all_non_zero_bufs();
+    if (bufs.empty()) {
+        return;
+    }
+
+    auto dev = param.stream->get_native_stream().get_device();
+    auto ctx = param.stream->get_native_stream().get_context();
+
+    std::set<sycl::usm::alloc> usm_types;
+    for (size_t idx = 0; idx < bufs.size(); idx++) {
+        usm_types.insert(sycl::get_pointer_type(bufs[idx], ctx));
+    }
+
+    if (usm_types.size() != 1) {
+        auto first_usm_type = *usm_types.begin();
+        auto second_usm_type = *(++usm_types.begin());
+        CCL_THROW("coll: ",
+                  ccl_coll_type_to_str(param.ctype),
+                  ", mixed USM pointer types (",
+                  ccl_usm_type_to_str(first_usm_type),
+                  ", ",
+                  ccl_usm_type_to_str(second_usm_type),
+                  ") within single operation are not supported, ",
+                  "device type: ",
+                  ccl_sycl_device_to_str(dev));
+    }
+
+    sycl::usm::alloc usm_type = *usm_types.begin();
+    bool is_valid_type = true;
+
+    if ((usm_type == sycl::usm::alloc::host) && (dev.is_gpu() || dev.is_accelerator()))
+        is_valid_type = false;
+
+    if ((usm_type == sycl::usm::alloc::device) && !(dev.is_gpu() || dev.is_accelerator()))
+        is_valid_type = false;
+
+    if (usm_type == sycl::usm::alloc::unknown)
+        is_valid_type = false;
+
+    LOG_DEBUG("coll: ",
+              ccl_coll_type_to_str(param.ctype),
+              ", USM pointer type: ",
+              ccl_usm_type_to_str(usm_type),
+              ", device type: ",
+              ccl_sycl_device_to_str(dev));
+
+    CCL_THROW_IF_NOT(is_valid_type,
+                     "coll: ",
+                     ccl_coll_type_to_str(param.ctype),
+                     " - invalid USM pointer type: ",
+                     ccl_usm_type_to_str(usm_type),
+                     " for device type: ",
+                     ccl_sycl_device_to_str(dev));
+}
+#endif /* CCL_ENABLE_SYCL */
+
+void ccl_coll_validate_user_input(const ccl_coll_param& param, const ccl_coll_attr& attr) {
+    CCL_THROW_IF_NOT(ccl::global_data::env().atl_transport == ccl_atl_ofi || !(attr.reduction_fn),
+                     "custom reduction is supported for OFI transport only");
+
+    CCL_THROW_IF_NOT(ccl_datatype_storage::is_predefined_datatype(param.dtype.idx()) ||
+                         ccl::global_data::env().atl_transport == ccl_atl_ofi,
+                     "custom datatype is supported for OFI transport only");
+
+    CCL_THROW_IF_NOT((param.ctype != ccl_coll_allreduce && param.ctype != ccl_coll_reduce &&
+                      param.ctype != ccl_coll_sparse_allreduce) ||
+                         ccl_datatype_storage::is_predefined_datatype(param.dtype.idx()) ||
+                         attr.reduction_fn,
+                     "custom datatype requires custom reduction");
+
+    CCL_THROW_IF_NOT(param.ctype == ccl_coll_allreduce ||
+                         !(attr.prologue_fn || attr.epilogue_fn || attr.reduction_fn),
+                     "prologue/epilogue/custom reduction is supported for allreduce only");
+
+    CCL_THROW_IF_NOT(param.ctype == ccl_coll_allgatherv || !(attr.vector_buf),
+                     "vector buffer is supported for allgatherv only");
+
+    if (param.ctype == ccl_coll_sparse_allreduce) {
+        CCL_THROW_IF_NOT(
+            ccl::global_data::env().sparse_allreduce_algo_raw != "mask" || !(attr.reduction_fn),
+            "mask algorithm for sparse_allreduce does not support custom reduction");
+
+        CCL_THROW_IF_NOT(
+            (attr.sparse_allreduce_completion_fn || attr.sparse_allreduce_alloc_fn) &&
+                !(reinterpret_cast<uintptr_t>(attr.sparse_allreduce_completion_fn) &
+                  reinterpret_cast<uintptr_t>(attr.sparse_allreduce_alloc_fn)),
+            "sparse_allreduce requires completion callback only or allocation callback only");
+    }
+
+    if (param.dtype.idx() == ccl::datatype::float16) {
+        CCL_THROW_IF_NOT(ccl::global_data::env().fp16_impl_type != ccl_fp16_no_compiler_support,
+                         "FP16 datatype is requested but not supported by CCL compiler");
+        CCL_THROW_IF_NOT(ccl::global_data::env().fp16_impl_type != ccl_fp16_no_hardware_support,
+                         "FP16 datatype is requested but not supported by hardware");
+    }
+
+    if (param.dtype.idx() == ccl::datatype::bfloat16) {
+        CCL_THROW_IF_NOT(ccl::global_data::env().bf16_impl_type != ccl_bf16_no_compiler_support,
+                         "BF16 datatype is requested but not supported by CCL compiler");
+        CCL_THROW_IF_NOT(ccl::global_data::env().bf16_impl_type != ccl_bf16_no_hardware_support,
+                         "BF16 datatype is requested but not supported by hardware");
+    }
+
+    if (param.ctype == ccl_coll_bcast || param.ctype == ccl_coll_reduce) {
+        CCL_THROW_IF_NOT(param.root < param.comm->size(),
+                         "unexpected root ",
+                         param.root,
+                         ", comm size ",
+                         param.comm->size());
+    }
+
+    if (param.stream) {
+#ifdef CCL_ENABLE_SYCL
+        /* SYCL specific validation */
+
+        /* TODO: compare stream dev/ctx and comm dev/ctx */
+        // sycl::device stream_dev = param.stream->get_native().get_context();
+        // sycl::device stream_ctx = param.stream->get_native().get_device();
+
+        if (!attr.is_sycl_buffer) {
+            /* check whether USM pointers have expected type */
+            ccl_check_usm_pointers(param);
+        }
+#endif /* CCL_ENABLE_SYCL */
+    }
+}
---
+++ src/sched/entry/copy/copy_helper.hpp
@@ -0,0 +1,165 @@
+/*
+ Copyright 2016-2020 Intel Corporation
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#pragma once
+
+#include "coll/coll_check.hpp"
+#include "common/datatype/datatype.hpp"
+#include "common/global/global.hpp"
+#include "common/utils/buffer.hpp"
+#include "common/utils/enums.hpp"
+#include "common/utils/tuple.hpp"
+#include "oneapi/ccl/native_device_api/interop_utils.hpp"
+
+enum class copy_direction { d2h, h2d, d2d };
+std::string to_string(copy_direction val);
+
+#ifdef CCL_ENABLE_SYCL
+
+struct sycl_copier {
+    sycl_copier() = default;
+    sycl_copier(copy_direction direction,
+                ccl_buffer in_buf,
+                ccl_buffer out_buf,
+                size_t count,
+                const ccl_datatype& dtype,
+                bool is_sycl_buffer = false,
+                size_t in_buf_offset = 0)
+            : direction(direction),
+              in_buf(in_buf),
+              out_buf(out_buf),
+              count(count),
+              dtype(dtype),
+              is_sycl_buffer(is_sycl_buffer),
+              in_buf_offset(in_buf_offset) {}
+
+    bool is_completed() {
+        return (e.get_info<sycl::info::event::command_execution_status>() ==
+                sycl::info::event_command_status::complete)
+                   ? true
+                   : false;
+    }
+
+    void set_queue(sycl::queue* external_q) {
+        q = external_q;
+        CCL_THROW_IF_NOT(q);
+    }
+
+    template <size_t index, class specific_sycl_buffer>
+    void invoke() {
+        if (index == (int)(dtype.idx())) {
+            LOG_DEBUG("visitor matched index: ",
+                      index,
+                      ", ccl: ",
+                      ccl::global_data::get().dtypes->name(dtype),
+                      ", in: ",
+                      __PRETTY_FUNCTION__);
+
+            size_t bytes = count * dtype.size();
+
+            void* in_buf_ptr = in_buf.get_ptr(bytes);
+            void* out_buf_ptr = out_buf.get_ptr(bytes);
+
+            if (direction == copy_direction::d2d) {
+                CCL_THROW_IF_NOT(!is_sycl_buffer, "D2D + SYCL buffer");
+                e = q->submit([&](sycl::handler& h) {
+                    h.memcpy(out_buf_ptr,
+                             static_cast<typename specific_sycl_buffer::value_type*>(in_buf_ptr) +
+                                 in_buf_offset,
+                             bytes);
+                });
+                return;
+            }
+
+            void* void_device_ptr = (direction == copy_direction::h2d) ? out_buf_ptr : in_buf_ptr;
+
+            if (!is_sycl_buffer) {
+                auto device_ptr_type = sycl::get_pointer_type(void_device_ptr, q->get_context());
+                CCL_THROW_IF_NOT(device_ptr_type == sycl::usm::alloc::device,
+                                 "unexpected USM type ",
+                                 ccl_usm_type_to_str(device_ptr_type),
+                                 " for device_ptr ",
+                                 void_device_ptr);
+            }
+
+            LOG_DEBUG("count: ",
+                      count,
+                      ", in_buf_offset: ",
+                      in_buf_offset,
+                      ", dtype_size: ",
+                      dtype.size(),
+                      ", bytes: ",
+                      bytes,
+                      ", direction: ",
+                      to_string(direction),
+                      ", in_buf_ptr: ",
+                      in_buf_ptr,
+                      ", out_buf_ptr: ",
+                      out_buf_ptr,
+                      ", device_ptr: ",
+                      void_device_ptr,
+                      ", is_sycl_buffer: ",
+                      (is_sycl_buffer) ? "yes" : "no");
+
+            if (is_sycl_buffer) {
+                /* cast device pointer into SYCL buffer */
+                specific_sycl_buffer* device_buf_ptr =
+                    static_cast<specific_sycl_buffer*>(void_device_ptr);
+
+                specific_sycl_buffer host_buf(
+                    static_cast<typename specific_sycl_buffer::value_type*>(
+                        (direction == copy_direction::h2d) ? in_buf_ptr : out_buf_ptr),
+                    count,
+                    sycl::property::buffer::use_host_ptr{});
+
+                e = q->submit([&](sycl::handler& h) {
+                    auto& src_buf = (direction == copy_direction::h2d) ? host_buf : *device_buf_ptr;
+                    auto& dst_buf = (direction == copy_direction::h2d) ? *device_buf_ptr : host_buf;
+                    auto src_buf_acc = src_buf.template get_access<sycl::access::mode::read>(
+                        h, count, in_buf_offset);
+                    auto dst_buf_acc = dst_buf.template get_access<sycl::access::mode::write>(h);
+                    h.copy(src_buf_acc, dst_buf_acc);
+                });
+            }
+            else {
+                /* don't do special cast, provided USM pointer can be used as is in copy kernel */
+                e = q->memcpy(out_buf_ptr,
+                              static_cast<typename specific_sycl_buffer::value_type*>(in_buf_ptr) +
+                                  in_buf_offset,
+                              bytes);
+            }
+        }
+        else {
+            LOG_TRACE("visitor skipped index: ",
+                      index,
+                      ", ccl: ",
+                      ccl::global_data::get().dtypes->name(dtype),
+                      ", in: ",
+                      __PRETTY_FUNCTION__);
+        }
+    }
+
+    copy_direction direction;
+    ccl_buffer in_buf;
+    ccl_buffer out_buf;
+    size_t count;
+    ccl_datatype dtype;
+    bool is_sycl_buffer;
+    sycl::queue* q;
+    size_t in_buf_offset;
+    sycl::event e;
+};
+
+#endif /* CCL_ENABLE_SYCL */
